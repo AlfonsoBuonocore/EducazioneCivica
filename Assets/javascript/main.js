7daa48caf8f7f6d49e8636c0187bc9614f57662a
AlfonsoBuonocore <80062345+AlfonsoBuonocore@users.noreply.github.com>
2023-01-09 12:57:12 +0100

    cv

--- Assets/javascript/main.js
+++ Assets/javascript/main.js
@@ -1,29 +1,23 @@
 // --------- theme --------- //
 
   const ThemeButton = document.getElementById('theme-button')
-  // vedo se nello storage del mio browser c'è salvato un attributo collegato al tema del sito
   const StorageTheme = localStorage.getItem('selected-theme')
 
-  // se nello storage ci sono informazioni corrispondente all'attributo di cui gli ho dato il nome fa dei settaggi in base allinformazione che ha ottenuto 
   if (StorageTheme) {
     if (StorageTheme == "light") {
-      // se l'informazione è light setto il data-theme del body a light e metto come icona dello switch-theme il sole togliendo la luna
       document.body.setAttribute("data-theme","light")
       ThemeButton.classList.remove("bi-moon-fill")
       ThemeButton.classList.add("bi-sun-fill")
     }else{
-      // invece se l'informazione è dark o altro non corrispondente a light setto il data-theme del body a dark e metto come icona dello switch-theme la luna togliendo il sole
       document.body.setAttribute("data-theme","dark")
       ThemeButton.classList.remove("bi-sun-fill")
       ThemeButton.classList.add("bi-moon-fill")
     }
   }
 
-  // attraverso due funzione attribuisco a due variabili che mi serviranno in seguito con il tema del sito e l'immagine dello swtch-theme corrispondente
   var Theme = getTheme()
   var Icon = getIcon()
 
-  // quando lo switch-theme viene cliccato questa funzione chiama un'altra funzione simile alla funzione dei settaggi dallo storage che setta il tema al cambiamento da parte dell'utente
   ThemeButton.addEventListener("click", () =>{
     setTheme(Theme);
     setNewInt()
@@ -33,12 +27,10 @@
     document.body.style.transition = "background .5s ease-in-out"
   }
 
-  // funzioni che mi permettono di recuperare il tema e l'icona correnti nel sito
-  // tema
   function getTheme(){
     return document.body.getAttribute("data-theme")
   }
-  // icona
+
   function getIcon(){
     return ThemeButton.classList.contains("bi-moon-fill") ? "bi-moon-fill" : "bi-sun-fill"
   }
@@ -55,7 +47,6 @@
     }
     Theme = getTheme();
     Icon = getIcon();
-    // setto nello stoage locale il mio attributo corrispondente al tema del sito in modo che nel caricamento succesivo della pagina il tema sara gia attivo
     localStorage.setItem('selected-theme',Theme)
   }
 
